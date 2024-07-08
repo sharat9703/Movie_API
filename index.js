a88d08de76daaae68768a9e1705c1bdcd50ad5a8
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 <sharath.yaragatti@yahoo.com>
2024-07-08 14:13:53 +0530

    formatted the code

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express();
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 app.use("/api/movies", movieRoute);
 
